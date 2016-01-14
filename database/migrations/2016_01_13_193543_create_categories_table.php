d38e69ac6f6dd77827ac87878b8bd2ef6b32d5f8
Alexander Abumov <abumov.alex@gmail.com>
2016-01-14 06:03:28 +0300

    Update 2016_01_13_193543_create_categories_table.php
    update migration

--- database/migrations/2016_01_13_193543_create_categories_table.php
+++ database/migrations/2016_01_13_193543_create_categories_table.php
@@ -10,19 +10,46 @@ class CreateCategoriesTable extends Migration {
    *
    * @return void
    */
-  public function up() {
-    Schema::create('categories', function(Blueprint $table) {
+  public function up()
+	{
+		Schema::create('categories', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name')->nullable();
+			$table->integer('parent_id')->nullable()->index();
+			$table->integer('inner_id')->nullable();
+			$table->integer('lft')->nullable()->index();
+			$table->integer('rgt')->nullable()->index();
+			$table->integer('depth')->nullable();
+			$table->timestamps();
+		});
 
-      $table->increments('id');
-      $table->string('name')->nullable();
-      $table->integer('parent_id')->nullable()->index();
-      $table->integer('inner_id')->nullable();
-      $table->integer('lft')->nullable()->index();
-      $table->integer('rgt')->nullable()->index();
-      $table->integer('depth')->nullable();
-      $table->timestamps();
-    });
-  }
+		// Insert some stuff
+    DB::table('categories')->insert(
+        array(
+            'name' => 'Root node 1',
+            'lft' => 1,
+            'rgt' => 2,
+            'depth' => 0,
+        )
+    );
+    DB::table('categories')->insert(
+        array(
+            'name' => 'Root node 2',
+            'lft' => 3,
+            'rgt' => 4,
+            'depth' => 0,
+        )
+    );
+    DB::table('categories')->insert(
+        array(
+            'name' => 'Root node 3',
+            'lft' => 5,
+            'rgt' => 6,
+            'depth' => 0,
+        )
+    );
+	}
 
   /**
    * Reverse the migrations.
